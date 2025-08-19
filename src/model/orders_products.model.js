3026bbe804c9e2a9d858516dbcff35bc36b24216
Stella40k <bernardstella0704@gmail.com>
2025-08-19 10:08:17 -0300

    lo poco que hice

--- src/model/orders_products.model.js
+++ src/model/orders_products.model.js
@@ -3,39 +3,22 @@ import { DataTypes } from "sequelize";
 import { Order } from "./orders.model";
 import { Product } from "./products.model.js";
 
-export const Worker = sequelize.define('Worker', {
- id:{
+export const PedidoProducto = sequelize.define('PedidoProducto', {
+ cantidad:{
     type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true 
- },
- name:{
-   type: DataTypes.STRING(100),
-   allowNull: false
- },
- last_name:{
-    type: DataTypes.STRING(100),
     allowNull: false
  },
- email:{
-   type: DataTypes.STRING(100),
-   unique: true,
+ precio_unitario:{
+   type: DataTypes.FLOAT,
    allowNull: false
- },
- password:{
-   type: DataTypes.STRING(100),
-   allowNull: false
- }, 
- rol:{
-    type: DataTypes.ENUM("cajero", "administrador", "limpieza", "repositor"),
-    allowNull: false,
- },
- activo:{
-  type: DataTypes.BOOLEAN,
-  defaultValue: true
- }
+}
 },{
   timestamps: false
 });
 
 //definicion de relaciones
+//relacion m:m entre orders(pedidos) y products(productos) a traves de esta tabla pq
+//un pedido puede tener muchos productos y un producto puede estar en muchos pedidos
+
+//un pedido puede tener muchos productos
+Order.hasMany(PedidoProducto, {through: 'orderId'});
